46a576652cdd2996cdb51de15469d6261d2fae05
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-13 15:04:03 +0530

    Add files via upload

---
+++ Hashmap.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Hashmap{
+    public static void main(String[] args) {
+        HashMap<String,String> a=new HashMap<>();
+        a.put("SNo","01");
+        a.put("Sname","muraja");
+        System.out.println(a);
+    }
+}
