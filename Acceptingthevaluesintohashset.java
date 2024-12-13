5ae01ae1053a666ebf0416dc490f847e700d7d23
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-13 13:40:08 +0530

    Add files via upload

---
+++ Acceptingthevaluesintohashset.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Acceptingthevaluesintohashset{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        HashSet<String> a=new HashSet<>();
+        int n=sc.nextInt();
+        for (int i=0;i<n;i++) {
+            String s=sc.next();
+            a.add(s);
+        }
+        System.out.println(a);
+    }
+}
